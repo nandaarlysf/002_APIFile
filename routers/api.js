0d48be9f77f028e1ea836ecd27fff086af689348
nandaarlysf <nandaarlys36@gmail.com>
2026-08-11 21:35:06 +0700

    menentukan api.js

--- routers/api.js
+++ routers/api.js
@@ -5,6 +5,7 @@ const komikController = require('../controller/komikController');
 const penulisController = require('../controller/penulisController');
 const genreController = require('../controller/genreController');
 const authMiddleware = require('../middleware/authMiddleware');
+const uploadMiddleware = require('../middleware/uploadMiddleware');
 
 router.post('/register', penulisController.register);
 router.post('/login', penulisController.login);
@@ -14,8 +15,8 @@ router.get('/komik', komikController.getAll);
 router.get('/genre', genreController.getAll);
 
 // Protected
-router.post('/komik', authMiddleware, komikController.create);
-router.put('/komik/:id', authMiddleware, komikController.update);
+router.post('/komik', authMiddleware, uploadMiddleware.single('gambar'), komikController.create);
+router.put('/komik/:id', authMiddleware, uploadMiddleware.single('gambar'), komikController.update);
 router.delete('/komik/:id', authMiddleware, komikController.remove);
 
 router.post('/genre', authMiddleware, genreController.create);
